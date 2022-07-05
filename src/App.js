5eafc18025e6f946251384e2c54a9a386aff5be3
Abhishek Kumar <shishodiaabhi8447@gmail.com>
2022-07-05 10:38:34 +0530

    10:38

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Dropdown from "./Components/Dropdown/Dropdown";
+import Submit from "./Submit";
 
 function App() {
   return (
     <div className="App">
      <Dropdown/>
-    
+    <Submit/>
     </div>
   );
 }
---
+++ src/Submit.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Submit() {
+    <form>
+  
+    <input
+    required={true}
+      type="submit"
+      value="Submit"
+      style={{align:"center",padding:"5px",margin:"10px 0px 0px 40rem",color:"white",background:"blue"}}
+      onClick={(event) => {
+        event.preventDefault();
+        
+        // props.clearForm();
+        // props.onFormSubmit(event);
+        // props.onFormSubmit1(event);
+        // props.onFormSubmit2(event);
+        // props.onFormSubmit3(event);
+        // props.onFinSubmit(event);
+
+        // props.getDb();
+        props.postDB(event);
+        window.location.reload();
+      }}
+    />
+  </form>
+}
+
+export default Submit
